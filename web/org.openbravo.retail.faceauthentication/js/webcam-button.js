cb09512a4734cac42bcc5cb9774abba66e40fa47
openbravo <openbravo@por0971.mydomain>
2019-11-08 10:47:04 +0100

    Include button in login window.
    Create message flor login button

--- web/org.openbravo.retail.faceauthentication/js/webcam-button.js
+++ web/org.openbravo.retail.faceauthentication/js/webcam-button.js
@@ -6,3 +6,63 @@
  * or in the legal folder of this module distribution.
  ************************************************************************************
  */
+
+/*global OB, enyo*/
+
+enyo.kind({
+  name: 'OB.OBPOSLogin.UI.Login.faceAuthenticationButton',
+  kind: 'OB.UI.ModalDialogButton',
+  i18nLabel: 'FA_FaceAuthenticationLbl',
+  tap: function() {
+    var me = this;
+    //FIXME-SKIN: Username element structure has changed since the new POS Skin
+    var loginuser = me.owner.$.formElementUsername
+        ? me.owner.$.formElementUsername.coreElement.getValue()
+        : me.owner.$.username.getValue(),
+      connectedCallback = function() {
+        OB.UTIL.showConfirmation.display(
+          '',
+          OB.I18N.getLabel('OBRETFP_PasswordRequestApproved')
+        );
+      },
+      notConnectedCallback = function() {
+        OB.UTIL.showError(
+          OB.I18N.getLabel('OBPOS_OfflineWindowRequiresOnline')
+        );
+        return;
+      };
+    if (loginuser === '') {
+      OB.UTIL.showError(OB.I18N.getLabel('OBRETFP_EmptyUser'));
+      return;
+    }
+    OB.UTIL.Approval.requestApproval(
+      me.model,
+      'OBRETFP_approval.forgotPassword',
+      function(approved, supervisor, approvalType) {
+        if (approved && supervisor) {
+          if (
+            OB.MobileApp.model.get('connectedToERP') ||
+            OB.UTIL.isNullOrUndefined(OB.MobileApp.model.get('connectedToERP'))
+          ) {
+            connectedCallback();
+          } else {
+            notConnectedCallback();
+          }
+        }
+      },
+      {
+        loginuser: loginuser,
+        terminal: OB.MobileApp.model.get('terminalName')
+      }
+    );
+  },
+  init: function(model) {
+    this.model = model;
+  }
+});
+
+// Push the new created button to buttons array at first position
+OB.OBPOSLogin.UI.Login.prototype.buttons.splice(0, 0, {
+  kind: 'OB.OBPOSLogin.UI.Login.faceAuthenticationButton',
+  name: 'faceAuthenticationButton'
+});
